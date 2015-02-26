2486a4e9392e649c5532011a13a18d303147b2c7
Will Kahn-Greene <willkg@mozilla.com>
2015-02-26 13:39:44 -0500

    Overhaul settings
    This allows for instance settings to come from local.py or the
    environment.
    
    It should parse environment variables correctly and also allow us to
    require values be set somewhere or throw an error.

--- harold/settings/__init__.py
+++ harold/settings/__init__.py
@@ -1,108 +1,28 @@
-"""
-Django settings for harold project.
+from harold.settings_utils import config, NO_VALUE
 
-For more information on this file, see
-https://docs.djangoproject.com/en/1.6/topics/settings/
+# Instance settings that are required
+SECRET_KEY = NO_VALUE
+SITE_URL = NO_VALUE
+DATABASES = NO_VALUE
 
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.6/ref/settings/
-"""
+# Pull in the default settings
+from harold.settings.defaults import *
+
+# Override with local.py
+try:
+    from harold.settings.local import *
+except ImportError:
+    pass
+
+# Instance settings which should come from the environment but can
+# come from local.py.
+
 import dj_database_url
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+SECRET_KEY = config('SECRET_KEY', override_value=SECRET_KEY, type_='str')
+SITE_URL = config('SITE_URL', override_value=SITE_URL, default='http://127.0.0.1:8000', type_='str')
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_b@zcmj=1h^sr#64!u3r+9d_=kvh+r3p5pbb0g42x-t0n2wbbi'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-TEMPLATE_DEBUG = True
-
-ALLOWED_HOSTS = ['*']
-
-AUTHENTICATION_BACKENDS = (
-    'django.contrib.auth.backends.ModelBackend',
-    'django_browserid.auth.BrowserIDBackend',
-)
-
-SITE_URL = os.environ.get('SITE_URL', 'http://127.0.0.1:8000')
-
-LOGIN_REDIRECT_URL = '/'
-LOGIN_REDIRECT_URL_FAILURE = '/'
-LOGOUT_REDIRECT_URL = '/'
-
-# Application definition
-
-INSTALLED_APPS = (
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-
-    'django_browserid',
-
-    'harold.base',
-)
-
-MIDDLEWARE_CLASSES = (
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-)
-
-ROOT_URLCONF = 'harold.urls'
-
-WSGI_APPLICATION = 'harold.wsgi.application'
-
-TEMPLATE_CONTEXT_PROCESSORS = (
-    "django.contrib.auth.context_processors.auth",
-    "django.core.context_processors.debug",
-    "django.core.context_processors.i18n",
-    "django.core.context_processors.media",
-    "django.core.context_processors.static",
-    "django.core.context_processors.tz",
-    "django.contrib.messages.context_processors.messages",
-
-    'django_browserid.context_processors.browserid',
-)
-
-# Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-
-DATABASES = {
-    'default': dj_database_url.config(default='sqlite:///harold.db')
-}
-
-# Internationalization
-# https://docs.djangoproject.com/en/1.6/topics/i18n/
-
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
-USE_I18N = True
-
-USE_L10N = True
-
-USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
-STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+if DATABASES is NO_VALUE:
+    DATABASES = {
+        'default': dj_database_url.config(default='sqlite:///harold.db')
+    }
