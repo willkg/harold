ef64ade332fd65b781d4cdd7156e5d0db88ea058
Will Kahn-Greene <willkg@mozilla.com>
2015-03-17 15:51:58 -0400

    Rework the test settings shortcut in manage.py
    This makes it possible to have test settings, but also use
    DJANGO_SETTINGS_MODULE without the two interfering with one another.

--- manage.py
+++ manage.py
@@ -4,15 +4,22 @@ import sys
 
 
 if __name__ == "__main__":
-    # If no DJANGO_SETTINGS_MODULE is specified, pick an appropriate
-    # one.
-    if not os.environ.get('DJANGO_SETTINGS_MODULE'):
-        if 'test' in sys.argv:
-            settings_module = 'harold.settings.test'
+    # If we're running tests, then we pull from
+    # DJANGO_TEST_SETTINGS_MODULE or use "harold.settings.test" and
+    # stomp on DJANGO_SETTINGS_MODULE.  This makes it easier to have
+    # separate test settings.
+    if 'test' in sys.argv:
+        if os.environ.get('DJANGO_TEST_SETTINGS_MODULE'):
+            settings_module = os.environ['DJANGO_TEST_SETTINGS_MODULE']
         else:
-            settings_module = 'harold.settings.base'
+            settings_module = 'harold.settings.test'
         os.environ['DJANGO_SETTINGS_MODULE'] = settings_module
-        
+
+    # If no DJANGO_SETTINGS_MODULE is specified thus far, pick an
+    # appropriate one.
+    if not os.environ.get('DJANGO_SETTINGS_MODULE'):
+        os.environ['DJANGO_SETTINGS_MODULE'] = 'harold.settings.base'
+
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
